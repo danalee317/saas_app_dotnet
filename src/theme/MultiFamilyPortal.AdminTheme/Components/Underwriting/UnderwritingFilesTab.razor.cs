218de09be46c5d14936a5aa1ff07dd42fc1eebc5
danalee317 <danalee317@outlook.com>
2021-11-10 21:39:49 -0800

    add Underwriting Files Tab

---
+++ src/theme/MultiFamilyPortal.AdminTheme/Components/Underwriting/UnderwritingFilesTab.razor.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Components;
+using MultiFamilyPortal.Dtos.Underwrting;
+
+namespace MultiFamilyPortal.AdminTheme.Components.Underwriting
+{
+    public partial class UnderwritingFilesTab
+    {
+        [Parameter]
+        public UnderwritingAnalysis Property { get; set; }
+
+        [Inject]
+        private NavigationManager _navigationManager { get; set; }
+
+        [Inject]
+        private HttpClient _client { get; set; }
+
+        private void CloseUpload()
+        {
+        }
+
+        public string SaveUrl => ToAbsoluteUrl($"api/admin/underwriting/upload/save/{Property.Id}");
+        public string RemoveUrl => ToAbsoluteUrl($"api/admin/underwriting/upload/remove/{Property.Id}");
+        public string ToAbsoluteUrl(string url)
+        {
+            return $"{_navigationManager.BaseUri}{url}";
+        }
+    }
+}

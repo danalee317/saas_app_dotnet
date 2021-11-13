9f94dee98d27bcea26b4bc59fc11ccc48fa0b06f
danalee317 <danalee317@outlook.com>
2021-11-13 11:27:48 -0800

    update Display of Email Templates

--- src/theme/MultiFamilyPortal.AdminTheme/Components/Settings/EmailTemplates.razor.cs
+++ src/theme/MultiFamilyPortal.AdminTheme/Components/Settings/EmailTemplates.razor.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Net.Http.Json;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Components;
 using MultiFamilyPortal.Collections;
 using MultiFamilyPortal.CoreUI;
@@ -14,6 +15,9 @@ namespace MultiFamilyPortal.AdminTheme.Components.Settings
         [Inject]
         private HttpClient _client { get; set; }
 
+        [Inject]
+        private NavigationManager _navigationManager { get; set; }
+
         private ObservableRangeCollection<EmailTemplate> _emailTemplates { get; set; } = new();
 
         private EmailTemplate _selected;
@@ -22,13 +26,16 @@ namespace MultiFamilyPortal.AdminTheme.Components.Settings
 
         protected override async Task OnInitializedAsync()
         {
-            _emailTemplates.ReplaceRange(await _client.GetFromJsonAsync<IEnumerable<EmailTemplate>>("/api/admin/settings/email-templates"));
+            await UpdateAsync();
         }
 
         private void OnEditItem(GridCommandEventArgs args)
         {
             _definitions = Array.Empty<TemplateVariableDefinition>();
             _selected = args.Item as EmailTemplate;
+
+            _selected.Html = Regex.Replace(_selected.Html, Regex.Escape("{{SiteUrl}}"), _navigationManager.BaseUri);
+
             var type = Type.GetType(_selected.Model);
             if (type is null)
                 return;
@@ -41,25 +48,35 @@ namespace MultiFamilyPortal.AdminTheme.Components.Settings
                 });
         }
 
-        private record TemplateVariableDefinition
-        {
-            public string Name { get; init; }
-            public bool PartialTemplate { get; init; }
-        }
-
         private async Task OnUpdateTemplate()
         {
             if (_selected is null)
                 return;
 
+            _selected.Html = Regex.Replace(_selected.Html, Regex.Escape(_navigationManager.BaseUri), "{{SiteUrl}}");
             using var response = await _client.PostAsJsonAsync("/api/admin/settings/email-templates/update", _selected);
 
             if (response.IsSuccessStatusCode)
+            {
                 notification.ShowSuccess("Email template was successfully updated");
+                _selected = null;
+                await UpdateAsync();
+            }
             else
+            {
                 notification.ShowError("Unable to update the email template");
+            }
+        }
 
-            _selected = null;
+        private async Task UpdateAsync()
+        {
+            _emailTemplates.ReplaceRange(await _client.GetFromJsonAsync<IEnumerable<EmailTemplate>>("/api/admin/settings/email-templates"));
+        }
+
+        private record TemplateVariableDefinition
+        {
+            public string Name { get; init; }
+            public bool PartialTemplate { get; init; }
         }
     }
 }

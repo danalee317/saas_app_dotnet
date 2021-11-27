a3baee3c7168a2cd58113c4c8583f260c5d0547b
danalee317 <danalee317@outlook.com>
2021-11-27 13:04:07 +0200

    User update
    User update and password reset.

--- src/MultiFamilyPortal/Areas/Admin/Controllers/UsersController.cs
+++ src/MultiFamilyPortal/Areas/Admin/Controllers/UsersController.cs
@@ -112,10 +112,10 @@ namespace MultiFamilyPortal.Areas.Admin.Controllers
 
             var user = new SiteUser(request.Email)
             {
-                Email = request.Email,
+                Email = request.Email.Trim(),
                 EmailConfirmed = true,
-                FirstName = request.FirstName,
-                LastName = request.LastName,
+                FirstName = request.FirstName.Trim(),
+                LastName = request.LastName.Trim(),
                 PhoneNumber = request.Phone,
                 PhoneNumberConfirmed = true,
             };
@@ -134,13 +134,13 @@ namespace MultiFamilyPortal.Areas.Admin.Controllers
                     password = await response.Content.ReadAsStringAsync();
                     result = await _userManager.CreateAsync(user, password);
                 }
-                catch  (Exception ex)
+                catch (Exception ex)
                 {
                     return StatusCode(500,ex);
                 }
 
-                info=$"Your password is <b> {password}</b>";
-                tip="You can change your password in your profile.";
+                info = $"Your password is <b> {password}</b>";
+                tip = "You can change your password in your profile.";
             }
             else
             {
@@ -180,7 +180,113 @@ namespace MultiFamilyPortal.Areas.Admin.Controllers
                 var template = await templateProvider.GetTemplate(PortalTemplate.ContactMessage, notification);
                 await emailSender.SendAsync(user.Email, template);
             }
+            
+            return Ok();
+        }
 
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditUser(string id, [FromBody] CreateUserRequest request)
+        {
+            // only deals with roles
+            var user = await _dbContext.Users.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (user == null)
+                return BadRequest();
+
+            try
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+                await _userManager.RemoveFromRolesAsync(user, roles.Select(x => x).ToArray());
+                await _dbContext.SaveChangesAsync();
+                await _userManager.AddToRolesAsync(user, request.Roles);
+
+                if (!request.Roles.Contains(PortalRoles.PortalAdministrator) && !request.Roles.Contains(PortalRoles.Underwriter))
+                {
+                    var goals = await _dbContext.UnderwriterGoals.FirstOrDefaultAsync(x => x.UnderwriterId == user.Id);
+                    if (goals != null)
+                    {
+                        _dbContext.UnderwriterGoals.Remove(goals);
+                        await _dbContext.SaveChangesAsync();
+                    }
+                }
+                else
+                {
+                    var goals = new UnderwriterGoal
+                    {
+                        UnderwriterId = user.Id,
+                    };
+
+                    await _dbContext.UnderwriterGoals.AddAsync(goals);
+                    await _dbContext.SaveChangesAsync();
+                }
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            var exists = await _dbContext.Users.AnyAsync(x => x.Id == id);
+
+            if (!exists)
+                return NotFound();
+
+            try
+            {
+                var user = await _dbContext.Users.FirstAsync(x => x.Id == id);
+                await _userManager.DeleteAsync(user);
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost("update/password")]
+        public async Task<IActionResult> UpdatePassword([FromBody] CreateUserRequest request, [FromServices] IEmailService emailSender, [FromServices] ITemplateProvider templateProvider)
+        {
+            if (request.UseLocalAccount)
+                try
+                {
+                    using var _client = new HttpClient();
+                    var response = await _client.GetAsync("https://www.passwordrandom.com/query?command=password");
+                    var password = await response.Content.ReadAsStringAsync();
+
+                    if (string.IsNullOrEmpty(password))
+                        return BadRequest();
+
+                    var siteTitle = await _dbContext.GetSettingAsync<string>(PortalSetting.SiteTitle);
+                    var info = $"Your password is <b> {password}</b>";
+                    var tip = "You can change your password in your profile.";
+                    var notification = new ContactFormEmailNotification
+                    {
+                        DisplayName = request.FirstName + " " + request.LastName,
+                        Email = request.Email,
+                        FirstName = request.FirstName,
+                        LastName = request.LastName,
+                        Message = $"<p>Your password at https://{HttpContext.Request.Host} with your this email address has been reset by the administrator. {info} .<br/>{tip}<br/></p>",
+                        SiteTitle = siteTitle,
+                        SiteUrl = $"https://{HttpContext.Request.Host}",
+                        Subject = $"{siteTitle} - New Account Created",
+                        Year = DateTime.Now.Year,
+                    };
+                    var template = await templateProvider.GetTemplate(PortalTemplate.ContactMessage, notification);
+                    await emailSender.SendAsync(request.Email, template);
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(500, ex);
+                }
+            else
+                return BadRequest();
             return Ok();
         }
     }

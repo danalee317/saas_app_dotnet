442cd40f6167384fb4ab1a40cf34b841255d7dba
danalee317 <danalee317@outlook.com>
2021-12-09 13:28:49 +0200

    Reactive numbers
    Simple changes.

--- src/theme/MultiFamilyPortal.AdminTheme/Components/Underwriting/UnderwritingSummary.razor.cs
+++ src/theme/MultiFamilyPortal.AdminTheme/Components/Underwriting/UnderwritingSummary.razor.cs
@@ -1,4 +1,3 @@
-
 using System.Reactive;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
@@ -6,11 +5,54 @@ using Microsoft.AspNetCore.Components;
 using MultiFamilyPortal.Dtos.Underwriting;
 using ReactiveUI;
 
-namespace MultiFamilyPortal.AdminTheme.Components.Underwriting
+namespace MultiFamilyPortal.AdminTheme.Components.Underwriting;
+
+public partial class UnderwritingSummary
 {
-    public partial class UnderwritingSummary
+    [Parameter]
+    public UnderwritingAnalysis Property { get; set; }
+
+    private double _capRate;
+    private double _debtCoverageRatio;
+    private double _sellerCashOnCash;
+
+    private ReactiveCommand<Unit, Unit> RefreshCommand;
+    private CompositeDisposable _disposables = new CompositeDisposable();
+
+    protected override void OnInitialized()
     {
-        [Parameter]
-        public UnderwritingAnalysis Property { get; set; }
+        RefreshCommand = ReactiveCommand.CreateFromTask(Refresh).DisposeWith(_disposables);
+        Property.WhenAnyValue(x => x.CapRate).Select(_ => Unit.Default).InvokeCommand(RefreshCommand).DisposeWith(_disposables);
+        Property.WhenAnyValue(x => x.DebtCoverage).Select(_ => Unit.Default).InvokeCommand(RefreshCommand).DisposeWith(_disposables);
+        Property.WhenAnyValue(x => x.SellerCashOnCash).Select(_ => Unit.Default).InvokeCommand(RefreshCommand).DisposeWith(_disposables);
+    }
+
+    private async Task Refresh()
+    {
+        if (_capRate != Property.CapRate)
+        {
+            _capRate = Property.CapRate;
+            await InvokeAsync(StateHasChanged);
+        }
+
+        if (_debtCoverageRatio != Property.DebtCoverage)
+        {
+            _debtCoverageRatio = Property.DebtCoverage;
+            await InvokeAsync(StateHasChanged);
+        }
+
+        if (_sellerCashOnCash != Property.SellerCashOnCash)
+        {
+            _sellerCashOnCash = Property.SellerCashOnCash;
+            await InvokeAsync(StateHasChanged);
+        }
+    }
+
+    public void Dispose()
+    {
+        if (!_disposables.IsDisposed)
+            _disposables.Dispose();
+
+        GC.SuppressFinalize(this);
     }
 }

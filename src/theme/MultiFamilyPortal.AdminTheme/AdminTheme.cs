70bc9d26d033557de0cb91012e8c2dccf999a900
danalee317 <danalee317@outlook.com>
2021-12-12 14:53:22 -0800

    Convert Admin Menu to use new Menu Provider

--- src/theme/MultiFamilyPortal.AdminTheme/AdminTheme.cs
+++ src/theme/MultiFamilyPortal.AdminTheme/AdminTheme.cs
@@ -1,15 +1,10 @@
-﻿using MultiFamilyPortal.Themes;
+﻿using Microsoft.AspNetCore.Components.Routing;
+using MultiFamilyPortal.Authentication;
+using MultiFamilyPortal.Themes;
 
 namespace MultiFamilyPortal.AdminTheme
 {
-    //public class AdminTheme : IPortalAdminTheme
-    //{    //    public Type Layout { get; } = typeof(MainLayout);    //    public string Name { get; } = "Admin Theme";
-
-    //    public Type _404 { get; } = typeof(Components._404);
-    //    public string[] RequiredStyles { get; } = new[] { "_content/MultiFamilyPortal.AdminTheme/css/site.css" };
-    //}
-
-    public class AdminTheme : SideBarTheme.SideBarTheme, IPortalAdminTheme
+    public class AdminTheme : SideBarTheme.SideBarTheme, IPortalAdminTheme, IMenuProvider
     {
         public AdminTheme()
         {
@@ -17,5 +12,117 @@ namespace MultiFamilyPortal.AdminTheme
 
         public override string Name { get; } = "Admin Theme";
         public override Type SideBar { get; } = typeof(Layouts.NavMenu);
+        public IEnumerable<RootMenuOption> Menu { get; } = new[]
+        {
+            new RootMenuOption
+            {
+                Title = "Dashboard",
+                IconClass = "fa-duotone fa-tachometer-alt-fastest",
+                Link = "/admin",
+                Match = NavLinkMatch.All,
+                Policy = PortalPolicy.AdminPortalViewer
+            },
+            new RootMenuOption
+            {
+                Title = "Content",
+                IconClass = "fa-duotone fa-pen-to-square",
+                Policy = PortalPolicy.Blogger,
+                Children = new []
+                {
+                    new ChildMenuOption
+                    {
+                        Title = "Pages",
+                        Link = "/admin/pages",
+                        RequiredRole = PortalRoles.PortalAdministrator
+                    },
+                    new ChildMenuOption
+                    {
+                        Title = "Blogs",
+                        Link = "/admin/posts"
+                    },
+                    new ChildMenuOption
+                    {
+                        Title = "New Post",
+                        Link = "/admin/create-post"
+                    }
+                }
+            },
+            new RootMenuOption
+            {
+                Title = "Activity Log",
+                IconClass = "fa-duotone fa-mobile",
+                Link = "/admin/activity-log",
+                Policy = PortalPolicy.UnderwritingViewer
+            },
+            new RootMenuOption
+            {
+                Title = "Properties",
+                IconClass = "fa-duotone fa-city",
+                Policy = PortalPolicy.UnderwritingViewer,
+                Children = new []
+                {
+                    new ChildMenuOption
+                    {
+                        Title = "Underwriting",
+                        Link = "/admin/properties/underwriting"
+                    },
+                    new ChildMenuOption
+                    {
+                        Title = "Portfolio",
+                        Link = "/admin/properties/assets-under-management",
+                        Policy = PortalPolicy.Underwriter
+                    }
+                }
+            },
+            new RootMenuOption
+            {
+                Title = "Users",
+                IconClass = "fa-duotone fa-users",
+                Policy = PortalPolicy.Underwriter,
+                Children = new []
+                {
+                    new ChildMenuOption
+                    {
+                        Title = "Investors",
+                        Link = "/admin/investors",
+                        Match = NavLinkMatch.All
+                    },
+                    new ChildMenuOption
+                    {
+                        Title = "Prospects",
+                        Link = "/admin/prospects",
+                        Match = NavLinkMatch.All
+                    },
+                    new ChildMenuOption
+                    {
+                        Title = "Website Users",
+                        Link = "/admin/users",
+                        Match = NavLinkMatch.All,
+                        RequiredRole = PortalRoles.PortalAdministrator
+                    },
+                    new ChildMenuOption
+                    {
+                        Title = "Subscribers",
+                        Link = "/admin/subscribers",
+                        Match = NavLinkMatch.All,
+                        RequiredRole = PortalRoles.PortalAdministrator
+                    }
+                }
+            },
+            new RootMenuOption
+            {
+                Title = "Profile",
+                IconClass = "fa-duotone fa-user",
+                Link = "/admin/user-profile",
+                Policy = PortalPolicy.AdminPortalViewer,
+            },
+            new RootMenuOption
+            {
+                Title = "Settings",
+                IconClass = "fa-duotone fa-cogs",
+                Link = "/admin/settings",
+                RequiredRole = PortalRoles.PortalAdministrator
+            }
+        };
     }
 }

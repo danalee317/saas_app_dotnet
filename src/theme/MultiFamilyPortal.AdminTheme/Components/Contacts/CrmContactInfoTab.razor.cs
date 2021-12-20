dfe08384114f2c50d474b07a932a487a617fd623
danalee317 <danalee317@outlook.com>
2021-12-19 23:10:34 -0800

    adding base Contact Info dialogs

--- src/theme/MultiFamilyPortal.AdminTheme/Components/Contacts/CrmContactInfoTab.razor.cs
+++ src/theme/MultiFamilyPortal.AdminTheme/Components/Contacts/CrmContactInfoTab.razor.cs
@@ -1,3 +1,4 @@
+using System.Net.Http.Json;
 using Microsoft.AspNetCore.Components;
 using MultiFamilyPortal.Data.Models;
 
@@ -7,5 +8,25 @@ namespace MultiFamilyPortal.AdminTheme.Components.Contacts
     {
         [Parameter]
         public CRMContact Contact { get; set; }
+
+        [Inject]
+        private HttpClient _client { get; set; }
+
+        private IEnumerable<CRMContactRole> _allRoles;
+        private List<Guid> _roles = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (Contact.Roles?.Any() ?? false)
+                _roles = new List<Guid>(Contact.Roles.Select(x => x.Id));
+
+            _allRoles = await _client.GetFromJsonAsync<IEnumerable<CRMContactRole>>("/api/admin/contacts/crm-roles");
+        }
+
+        private void OnRolesUpdated(List<Guid> roles)
+        {
+            _roles = roles;
+            Contact.Roles = _allRoles.Where(x => roles.Contains(x.Id)).ToList();
+        }
     }
 }

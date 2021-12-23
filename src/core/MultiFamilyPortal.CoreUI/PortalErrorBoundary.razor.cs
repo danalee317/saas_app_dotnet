b4e40eca9cbbae6f00658c07ae6cf97a0bc921d7
danalee317 <danalee317@outlook.com>
2021-12-22 16:22:14 -0800

    add Logging for Handled Errors

--- src/core/MultiFamilyPortal.CoreUI/PortalErrorBoundary.razor.cs
+++ src/core/MultiFamilyPortal.CoreUI/PortalErrorBoundary.razor.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.Logging;
 
 namespace MultiFamilyPortal.CoreUI
 {
@@ -8,10 +9,18 @@ namespace MultiFamilyPortal.CoreUI
         [Parameter]
         public RenderFragment ChildContent { get; set; }
 
+        [Inject]
+        private ILogger<PortalErrorBoundary> _logger { get; set; }
+
         private ErrorBoundary errorBoundary;
         private void RecoverFromError()
         {
             errorBoundary?.Recover();
+        }
+
+        private void HandleError(Exception ex)
+        {
+            _logger.LogError(ex, "Unhandled exception caught by Error Boundary");
         }
     }
 }

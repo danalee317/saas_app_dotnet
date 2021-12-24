65ebae621ebdf66f373b38c7d25ef916299bacfe
danalee317 <danalee317@outlook.com>
2021-12-24 09:08:35 -0800

    adding suspended tenant theme

---
+++ src/theme/MultiFamilyPortal.SuspendedTenantTheme/SuspendedTenantTheme.cs
@@ -0,0 +1,12 @@
+﻿using MultiFamilyPortal.Themes;
+
+namespace MultiFamilyPortal.SuspendedTenantTheme
+{
+    public class SuspendedTenantTheme : IPortalTheme
+    {
+        public Type Layout { get; } = typeof(Layouts.MainLayout);
+        public string Name { get; } = "Suspended";
+        public Type _404 { get; }
+        public string[] RequiredStyles { get; } = new string[] { };
+    }
+}

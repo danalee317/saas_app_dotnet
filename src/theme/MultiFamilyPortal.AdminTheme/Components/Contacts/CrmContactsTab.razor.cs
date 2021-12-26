a0911472cd2b1c88b21464b87030b59b70c21116
danalee317 <danalee317@outlook.com>
2021-12-25 22:08:27 -0800

    add contact filtering

--- src/theme/MultiFamilyPortal.AdminTheme/Components/Contacts/CrmContactsTab.razor.cs
+++ src/theme/MultiFamilyPortal.AdminTheme/Components/Contacts/CrmContactsTab.razor.cs
@@ -14,19 +14,31 @@ namespace MultiFamilyPortal.AdminTheme.Components.Contacts
         [Inject]
         private NavigationManager _navigationManager { get; set; }
 
+        private IEnumerable<CRMContact> _allContacts;
         private readonly ObservableRangeCollection<CRMContact> _contacts = new();
+        private readonly ObservableRangeCollection<CRMContactRole> _roles = new();
         private bool _showList = true;
         private CRMContact _newContact = null;
+        private string _query;
+        private string _selectedRole;
 
         protected override async Task OnInitializedAsync()
         {
+            var roles = await _client.GetFromJsonAsync<IEnumerable<CRMContactRole>>("/api/admin/contacts/crm-roles");
+            _roles.Clear();
+            _roles.Add(new CRMContactRole
+            {
+                Name = "All"
+            });
+            _roles.AddRange(roles);
+            _selectedRole = _roles.First().Name;
             await UpdateContacts();
         }
 
         private async Task UpdateContacts()
         {
-            var contacts = await _client.GetFromJsonAsync<IEnumerable<CRMContact>>("/api/admin/contacts/crm-contacts");
-            _contacts.ReplaceRange(contacts.OrderBy(x => x.LastName));
+            _allContacts = await _client.GetFromJsonAsync<IEnumerable<CRMContact>>("/api/admin/contacts/crm-contacts");
+            FilterResults();
         }
 
         private void AddNewContact()
@@ -51,6 +63,33 @@ namespace MultiFamilyPortal.AdminTheme.Components.Contacts
         private void ShowContactDetails(CRMContact contact)
         {
             _navigationManager.NavigateTo($"/admin/contacts/detail/{contact.Id}");
+        }
+
+        private void OnQueryChanged(string query)
+        {
+            _query = query;
+            FilterResults();
+        }
+
+        private void OnRoleTypeChanged()
+        {
+            FilterResults();
+        }
+
+        private void FilterResults()
+        {
+            var filtered = _allContacts;
+            if(!string.IsNullOrEmpty(_query))
+                filtered = filtered.Where(x =>
+                    x.FirstName.Contains(_query, StringComparison.InvariantCultureIgnoreCase) ||
+                    x.LastName.Contains(_query, StringComparison.InvariantCultureIgnoreCase) ||
+                    x.Company.Contains(_query, StringComparison.InvariantCultureIgnoreCase) ||
+                    x.Emails.Any(e => e.Email.Contains(_query, StringComparison.InvariantCultureIgnoreCase)));
+
+            if (_roles.First(x => x.Name == _selectedRole).Id != default)
+                filtered = filtered.Where(x => x.Roles.Any(x => x.Name == _selectedRole));
+
+            _contacts.ReplaceRange(filtered.OrderBy(x => x.LastName));
         }
     }
 }

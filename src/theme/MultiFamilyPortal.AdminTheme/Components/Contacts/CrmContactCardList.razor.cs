9c1cf69584f02732bb50d8feee151951f29ff90d
danalee317 <danalee317@outlook.com>
2022-01-04 08:38:39 +0200

    conventions

--- src/theme/MultiFamilyPortal.AdminTheme/Components/Contacts/CrmContactCardList.razor.cs
+++ src/theme/MultiFamilyPortal.AdminTheme/Components/Contacts/CrmContactCardList.razor.cs
@@ -14,7 +14,7 @@ namespace MultiFamilyPortal.AdminTheme.Components.Contacts
         [Parameter]
         public EventCallback<CRMContact> ShowContactDetails { get; set; }
 
-        public int _pageSize { get; set; } = 6;
+        public int _pageSize { get; set; } = 15;
         public int _page { get; set; } = 1; 
     }
 }

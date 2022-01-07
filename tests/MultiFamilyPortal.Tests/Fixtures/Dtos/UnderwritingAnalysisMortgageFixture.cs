7914f49740c4100c3700781631a3b11861d418f7
danalee317 <danalee317@outlook.com>
2022-01-06 18:53:16 -0800

    add mortgage tests

---
+++ tests/MultiFamilyPortal.Tests/Fixtures/Dtos/UnderwritingAnalysisMortgageFixture.cs
@@ -0,0 +1,42 @@
+﻿using MultiFamilyPortal.Dtos.Underwriting;
+using Xunit;
+
+namespace MultiFamilyPortal.Tests.Fixtures.Dtos
+{
+    public class UnderwritingAnalysisMortgageFixture
+    {
+        [Fact]
+        public void CalculatesPIPayment()
+        {
+            var mortgage = new UnderwritingAnalysisMortgage
+            {
+                LoanAmount = 1000000
+            };
+
+            Assert.Equal(57289.84, mortgage.AnnualDebtService);
+        }
+
+        [Fact]
+        public void CalculatesIOPayment()
+        {
+            var mortgage = new UnderwritingAnalysisMortgage
+            {
+                LoanAmount = 1000000,
+                InterestOnly = true,
+            };
+
+            Assert.Equal(40000, mortgage.AnnualDebtService);
+        }
+
+        [Fact]
+        public void CalculatesPointCost()
+        {
+            var mortgage = new UnderwritingAnalysisMortgage
+            {
+                LoanAmount = 1000000
+            };
+
+            Assert.Equal(10000, mortgage.PointCost);
+        }
+    }
+}
